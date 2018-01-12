a4dfba175ae5f9e50464523b582e36ef05bc2015
omkar mote <omkarmote210@gmail.com>
2018-01-12 13:30:49 +0530

    displayed the login page

--- server.js
+++ server.js
@@ -5,6 +5,44 @@ const WebApp = require('./webapp.js');
 
 const app=WebApp.create();
 
+const headers={
+  ".jpg":"img/jpg",
+  ".html":"text/html",
+  ".css":"text/css",
+  ".js":"text/javascript",
+  ".gif":"img/gif",
+  ".pdf":"text/pdf"
+};
+
+let registeredUsers=[
+  {username:'omkar',password:'omkar'},
+  {username:'ketan',password:'ketan'}
+];
+
+const doesExist=req=>{
+  return fs.existsSync('./public'+req.url);
+}
+
+const setHeader=function(req,res){
+  let file=".."+req.url;
+  let extensionOfFile=file.slice(file.lastIndexOf("."));
+  res.setHeader("Content-type",headers[extensionOfFile]);
+};
+
+const serveFile=function(req,res){
+  try {
+    if(doesExist(req)){
+      setHeader(req,res);
+      res.write(fs.readFileSync('./public'+req.url));
+      res.end();
+    }
+  } catch (e) {
+    return e;
+  }
+}
+
+app.use(serveFile);
+
 const PORT=9000;
 const server=http.createServer(app);
 server.listen(PORT);
