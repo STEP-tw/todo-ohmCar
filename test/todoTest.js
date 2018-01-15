c4755a22f6b01533d1540544f2e29b9238f3b4ec
omkar mote <omkarmote210@gmail.com>
2018-01-15 10:43:59 +0530

    added getTitle and getDescription functions to todo module

--- test/todoTest.js
+++ test/todoTest.js
@@ -4,11 +4,29 @@ const Todo=require('../lib/todo.js');
 
 describe('todo',()=>{
   describe('#getAllTodoItems',()=>{
-    it('should get all the todo items user having',done=>{
+    it('should return all the todo items user having',done=>{
       let todo=new Todo(
         'Washing Clothes','need to wash clothes on every sunday',{1:"washing white shirt"}
       );
       assert.deepEqual(todo.getAllTodoItems(),["washing white shirt"]);
+      done();
+    });
+  });
+  describe('#getTitle',()=>{
+    it('should return the todo title',done=>{
+      let todo=new Todo(
+        'Washing Clothes','need to wash clothes on every sunday',{1:"washing white shirt"}
+      );
+      assert.equal(todo.getTitle(),'Washing Clothes');
+      done();
+    });
+  });
+  describe('#getAllTodoItems',()=>{
+    it('should return the description of the todo',done=>{
+      let todo=new Todo(
+        'Washing Clothes','need to wash clothes on every sunday',{1:"washing white shirt"}
+      );
+      assert.equal(todo.getDescription(),'need to wash clothes on every sunday');
       done();
     });
   });
