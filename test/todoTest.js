a47d97a6e28c4d2637d79ca7b41b0d989c0e7b57
omkar mote <omkarmote210@gmail.com>
2018-01-15 11:08:15 +0530

    added a behaviour in todo module to delete and add items

--- test/todoTest.js
+++ test/todoTest.js
@@ -3,6 +3,7 @@ const assert=chai.assert;
 const Todo=require('../lib/todo.js');
 
 describe('todo',()=>{
+
   describe('#getAllTodoItems',()=>{
     it('should return all the todo items user having',done=>{
       let todo=new Todo(
@@ -12,6 +13,7 @@ describe('todo',()=>{
       done();
     });
   });
+
   describe('#getTitle',()=>{
     it('should return the todo title',done=>{
       let todo=new Todo(
@@ -21,12 +23,40 @@ describe('todo',()=>{
       done();
     });
   });
+
   describe('#getAllTodoItems',()=>{
     it('should return the description of the todo',done=>{
       let todo=new Todo(
         'Washing Clothes','need to wash clothes on every sunday',{1:"washing white shirt"}
       );
       assert.equal(todo.getDescription(),'need to wash clothes on every sunday');
+      done();
+    });
+  });
+
+  describe('#addTodoItems',()=>{
+    it('should add the todoItems',done=>{
+      let todo=new Todo(
+        'Washing Clothes','need to wash clothes on every sunday',{1:"washing white shirt"}
+      );
+      assert.deepEqual(todo.getAllTodoItems(),["washing white shirt"]);
+      todo.addTodoItems('washing black shirt');
+      assert.deepEqual(todo.getAllTodoItems(),["washing white shirt","washing black shirt"]);
+      done();
+    });
+  });
+
+  describe('#deleteTodoItems',()=>{
+    it('should add the todoItems',done=>{
+      let todo=new Todo(
+        'Washing Clothes','need to wash clothes on every sunday',{
+          1:"washing white shirt",
+          2:"washing black shirt"
+        }
+      );
+      assert.deepEqual(todo.getAllTodoItems(),["washing white shirt","washing black shirt"]);
+      todo.deleteTodoItem(2);
+      assert.deepEqual(todo.getAllTodoItems(),["washing white shirt"]);
       done();
     });
   });
